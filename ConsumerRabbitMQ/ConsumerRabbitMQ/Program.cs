6be595b4fbc2bc4c0859ad0fa60280bf0f9bf71b
ramons <ramoncssantos@gmail.com>
2019-08-03 17:01:33 -0300

    first rabbit to practicing

---
+++ ConsumerRabbitMQ/ConsumerRabbitMQ/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace ConsumerRabbitMQ
+{
+    class Program
+    {
+        static void Main()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "TestRabbitMQ",
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null);
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine(message);
+                    };
+                    channel.BasicConsume(queue: "TestRabbitMQ",
+                        autoAck: true,
+                        consumer: consumer);
+
+                    Console.WriteLine("Consumer Subiu!");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
