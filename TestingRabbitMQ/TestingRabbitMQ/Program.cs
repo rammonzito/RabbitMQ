f44f7fbe4ab9cedb0688f3a32e892be74426249f
ramons <ramoncssantos@gmail.com>
2019-08-03 16:50:18 -0300

    testing RabbitMQ

---
+++ TestingRabbitMQ/TestingRabbitMQ/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace TestingRabbitMQ
+{
+    class Enviar
+    {
+        static void Main()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "TestRabbitMQ",
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null);
+
+                    string message = "First Local RabbitMQ";
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "",
+                        routingKey: "TestRabbitMQ",
+                        basicProperties: null,
+                        body: body);
+                    Console.WriteLine("Mensagem Enviada!");
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
